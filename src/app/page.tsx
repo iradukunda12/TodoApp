9bc977dc9f9aa77ca7108efdd919af97576ec33f
Iradukunda12 <iradukundakvn8@gmail.com>
2025-03-23 10:35:24 +0200

    chore: installed the shadcn ui and redux for state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
+  const triggerButton = () => {
+    console.log("You clicked shadcn ui button components");
+  }
   return (
     <main className="w-screen h-screen bg-slate-500 flex flex-col items-center justify-center">
-      <h1 className="text-3xl text-white font-thin ">Next.js & Docker Todo App 🚀</h1>
+      <Button onClick={triggerButton}>Next.js & Docker Todo App 🚀</Button>
     </main>
   );
 }
