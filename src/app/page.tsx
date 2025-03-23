345d96f2c74cf67c3fa10674c2195900532c4470
Iradukunda12 <iradukundakvn8@gmail.com>
2025-03-23 13:03:25 +0200

    feat(ui): designed the reusable components and the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,36 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+
+
+
 
 export default function Home() {
   const triggerButton = () => {
     console.log("You clicked shadcn ui button components");
   }
+
   return (
-    <main className="w-screen h-screen bg-slate-500 flex flex-col items-center justify-center">
-      <Button onClick={triggerButton}>Next.js & Docker Todo App 🚀</Button>
+<>
+  <div className="w-screen min-h-screen bg-black flex flex-col justify-between items-center px-2">
+    <main className="flex flex-col justify-center items-center flex-1">
+      <div className="w-full max-w-[413px] h-[200px] px-3 flex flex-col justify-center items-center gap-4 text-black">
+        <h1 className="text-2xl text-white font-extralight text-center">
+          Stay Organized with Your To-Do List
+        </h1>
+        <p className="text-muted-foreground text-white font-extralight text-center leading-6">
+          Manage your tasks efficiently and boost productivity. Add, edit, and complete tasks with ease.
+        </p>
+      </div>
+      <Button className="bg-white hover:bg-white text-black font-extralight mt-4" onClick={triggerButton}>
+        Get Started Todo App 🚀
+      </Button>
     </main>
+    <footer className="text-center text-white py-4 ">
+       Copyright © By Nextjs {new Date().getFullYear()}
+    </footer>
+  </div>
+</>
+
   );
 }
